b898d0c90066d8a9c5e400fea0c37fb127efdb52
Nate Moore <nate@natemoo.re>
2020-07-13 07:18:39 -0400

    add cache-control, fix moduleResolution

--- api/now-playing.ts
+++ api/now-playing.ts
@@ -21,8 +21,7 @@ export default async function (req: NowRequest, res: NowResponse) {
   }
 
   res.setHeader("Content-Type", "image/svg+xml");
-  // res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
-  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
 
   const duration = item.duration_ms;
   const track = item.name;
