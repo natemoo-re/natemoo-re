757f880107a26ab021a844d9ac1d9263b9039ebc
Nate Moore <nate@natemoo.re>
2020-07-13 21:52:46 -0400

    add RAINBOW!

--- components/Block.tsx
+++ components/Block.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { colors } from '../utils/colors';
+import { colors, colorNames } from '../utils/colors';
 const size = 32;
 
 
@@ -12,7 +12,29 @@ const Block = ({ color }: { color: keyof typeof colors }) => {
         viewBox={`0 0 ${size} ${size}`}
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect x="0" y="0" width={size} height={size} fill={colors[color]} />
+        {color === "RAINBOW" && (
+          <style>{`
+          #block {
+            animation: rainbow 4s ease-in-out infinite;
+          }
+
+          @keyframes rainbow {
+            ${colorNames.slice(0, -1).map((name, i, { length: total }) => {
+              return `${Math.floor((i / (total - 1)) * 100)}% { 
+                fill: ${colors[name]};
+              }`;
+            })
+          }
+        `}</style>
+        )}
+        <rect
+          id="block"
+          x="0"
+          y="0"
+          width={size}
+          height={size}
+          fill={colors[color === "RAINBOW" ? "red" : color]}
+        />
       </svg>
     );
 }
--- utils/colors.ts
+++ utils/colors.ts
@@ -9,6 +9,7 @@ export const colors = {
   navy: "#2267CD",
   purple: "#8621D0",
   pink: "#FF8676",
+  RAINBOW: "RAINBOW"
 };
 
 export const colorNames = Object.keys(colors);
