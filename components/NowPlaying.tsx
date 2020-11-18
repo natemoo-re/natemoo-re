934981c8b6efb23d11c4650dc79ae9c361458f21
Nate Moore <nate@natemoo.re>
2020-11-18 12:32:01 -0600

    fix: progress bar padding

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -46,7 +46,7 @@ export const Player: React.FC<Props> = ({
             .progress-bar {
               position: relative;
               width: 100%;
-              max-width: 415px;
+              max-width: 360px;
               height: 4px;
               margin: -1px;
               border: 1px solid #e1e4e8;
