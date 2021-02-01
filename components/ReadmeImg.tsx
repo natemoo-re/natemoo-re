a58a12ccf4f80c94ced94c99ba146f4317234bd8
Nate Moore <nate@natemoo.re>
2021-01-31 18:48:06 -0600

    feat: add support for dark mode

--- components/ReadmeImg.tsx
+++ components/ReadmeImg.tsx
@@ -16,6 +16,21 @@ const ReadmeImg = ({ width, height, children }) => {
                 margin: 0;
                 box-sizing: border-box;
               }
+              :root {
+                color: rgb(36, 41, 46);
+                background: #FFF;
+              }
+
+              @media (prefers-color-scheme: dark) {
+                :root {
+                  color: rgb(201, 209, 217);
+                  background: rgb(13, 17, 23);
+                }
+
+                p {
+                  color: inherit !important;
+                }
+              }
             `}</style>
           {children}
         </div>
