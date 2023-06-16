ca525a3cde908738addac5eab3e87b030bea4aba
soravitpuengsema <s6301012630184@email.kmutnb.ac.th>
2023-06-17 03:09:04 +0700

    reorder the album list. order by album count from top 50 track

--- api/top-albums.ts
+++ api/top-albums.ts
@@ -17,28 +17,48 @@ export default async function (req: NowRequest, res: NowResponse) {
 
   const albumImages = items.map((item) => item.album.images);
 
+  const albumTrackCounts = {};
+  albumNames.forEach(function (x) { albumTrackCounts[x] = (albumTrackCounts[x] || 0) + 1; });
+  
+  let albumSorts = [];
+  for (let name in albumTrackCounts) {
+    albumSorts.push([name, albumTrackCounts[name]]);
+  }
+  albumSorts.sort(function(a, b) {
+      return b[1] - a[1];
+  });
+
   const uniqueData1 = [...new Set(albumNames)]; // Remove duplicates from data1
 
-  const result = uniqueData1.reduce((obj, key) => {
+  const albumInfo = uniqueData1.reduce((obj, key) => {
   const index = albumNames.lastIndexOf(key); // Find last index of key in data1
     obj[Object.keys(obj).length + 1] = [key, artistNames[index], albumYears[index], albumImages[index]];
     return obj;
   }, {});
 
-  const albumInfo = result[i]
+  for (let i = 0; i < albumSorts.length; i++) {
+    const albumName = albumSorts[i][0];
+  
+    for (const key in albumInfo) {
+      if (albumInfo.hasOwnProperty(key) && albumInfo[key][0] === albumName) {
+        albumSorts[i] = albumInfo[key];
+        break;
+      }
+    }
+  }
 
-  console.log(albumInfo)
+  console.log(albumSorts)
 
-  const cover = result[i][3][result[i][3].length - 1]?.url;
+  const cover = albumSorts[Number.parseInt(i)-1][3][albumSorts[i][3].length - 1]?.url;
   let coverImg = null;
   if (cover) {
     const buff = await (await fetch(cover)).arrayBuffer();
     coverImg = `data:image/jpeg;base64,${Buffer.from(buff).toString("base64")}`;
   }
 
-  const albumName = result[i][0]
-  const albumArtist = result[i][1]
-  const albumYear = result[i][2]
+  const albumName = albumSorts[Number.parseInt(i)-1][0]
+  const albumArtist = albumSorts[Number.parseInt(i)-1][1]
+  const albumYear = albumSorts[Number.parseInt(i)-1][2]
 
   const text = renderToString(
     Album({ index: Number.parseInt(i), cover: coverImg, albumName, albumArtist, albumYear })
