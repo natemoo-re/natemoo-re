a6886674d65049e79d031166a0a42b73c97f7ac3
soravitpuengsema <s6301012630184@email.kmutnb.ac.th>
2023-06-17 12:10:38 +0700

    change track url to album url

--- api/top-albums.ts
+++ api/top-albums.ts
@@ -8,18 +8,19 @@ export default async function (req: NowRequest, res: NowResponse) {
   i = Array.isArray(i) ? i[0] : i;
 
   const items = await topTrackAlbum({});
-
+  //map each value to each array
   const albumNames = items.map((item) => item.album.name);
-
   const artistNames = items.map((item) => (item.artists || []).map(({ name }) => name).join(", "));
-
   const albumYears = items.map((item) => item.album.release_date.split("-")[0]);
-
   const albumImages = items.map((item) => item.album.images);
+  const albumUrls = items.map((item) => item.album.external_urls);
+  const albumSpotifyUrls = items.map((item) => item.album.external_urls.spotify);
 
+  //array contains track count of the album
   const albumTrackCounts = {};
   albumNames.forEach(function (x) { albumTrackCounts[x] = (albumTrackCounts[x] || 0) + 1; });
-  
+
+  //sort albumTrackCounts by track count
   let albumSorts = [];
   for (let name in albumTrackCounts) {
     albumSorts.push([name, albumTrackCounts[name]]);
@@ -28,17 +29,17 @@ export default async function (req: NowRequest, res: NowResponse) {
       return b[1] - a[1];
   });
 
-  const uniqueData1 = [...new Set(albumNames)]; // Remove duplicates from data1
-
-  const albumInfo = uniqueData1.reduce((obj, key) => {
-  const index = albumNames.lastIndexOf(key); // Find last index of key in data1
-    obj[Object.keys(obj).length + 1] = [key, artistNames[index], albumYears[index], albumImages[index]];
+  //remove duplicates (tracks from the same album) and match album value from the same index
+  const uniqueAlbum = [...new Set(albumNames)];
+  const albumInfo = uniqueAlbum.reduce((obj, key) => {
+  const index = albumNames.lastIndexOf(key);
+    obj[Object.keys(obj).length + 1] = [key, artistNames[index], albumYears[index], albumImages[index], albumUrls[index], albumSpotifyUrls[index]];
     return obj;
   }, {});
 
-  for (let i = 0; i < albumSorts.length; i++) {
+  //if albumName match the album name from albumInfo, append the other value
+  for (let i = 0; i < albumSorts.length; i++) { // 
     const albumName = albumSorts[i][0];
-  
     for (const key in albumInfo) {
       if (albumInfo.hasOwnProperty(key) && albumInfo[key][0] === albumName) {
         albumSorts[i] = albumInfo[key];
@@ -46,8 +47,6 @@ export default async function (req: NowRequest, res: NowResponse) {
       }
     }
   }
-
-  console.log(albumSorts)
 
   const cover = albumSorts[Number.parseInt(i)-1][3][albumSorts[i][3].length - 1]?.url;
   let coverImg = null;
@@ -59,6 +58,8 @@ export default async function (req: NowRequest, res: NowResponse) {
   const albumName = albumSorts[Number.parseInt(i)-1][0]
   const albumArtist = albumSorts[Number.parseInt(i)-1][1]
   const albumYear = albumSorts[Number.parseInt(i)-1][2]
+  const albumUrl = albumSorts[Number.parseInt(i)-1][3]
+  const albumSpotifyUrl = albumSorts[Number.parseInt(i)-1][4] 
 
   const text = renderToString(
     Album({ index: Number.parseInt(i), cover: coverImg, albumName, albumArtist, albumYear })
@@ -69,9 +70,9 @@ export default async function (req: NowRequest, res: NowResponse) {
   }
 
   if (typeof open !== "undefined") {
-    if (items[i] && items[i].external_urls) {
+    if (albumSorts[Number.parseInt(i)-1] && albumUrl) {
       res.writeHead(302, {
-        Location: items[i].external_urls.spotify,
+        Location: albumSpotifyUrl,
       });
       return res.end();
     }
